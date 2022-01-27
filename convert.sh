4cad635fe7bbd02628d6076efb14d591b82d7fda
Pencinta Kucing <hotfloppy.6866@gmail.com>
2022-01-27 19:33:00 +0800

    added minor changes

--- convert.sh
+++ convert.sh
@@ -10,7 +10,7 @@ for file in ${filelist[@]}; do
   echo
   echo "Converting $file into HTML, PDF and PPTX.."
   filename=${file%.*}
-  marp --html "$file"--output ${filename}.html
+  marp --html "$file" --output ${filename}.html
   marp --pdf "$file" --output ${filename}.pdf
   marp --pptx "$file" --output ${filename}.pptx
   echo "Conversion for $file is done."
--- index.md
+++ index.md
@@ -10,11 +10,8 @@ marp: true
 
 ![DragonForce Malaysia](https://i.imgur.com/tL95KMG.png)
 
-[Bab 1:
-Intro to Linux](./intro-linux.html)
+[Bab 1: Intro to Linux](./intro-linux.html)
 
-[Bab 2:
-Linux CLI](./linux-cli.html)
+[Bab 2: Linux CLI](./linux-cli.html)
 
-[Bab 3:
-Shell Scripting](./shell-script.html)
+[Bab 3: Shell Scripting](./shell-script.html)
